5a10d0cfb14c03d341fa6714c5d214cb2381136b
Abilkhair Seilov <abilkhairseilov@outlook.com>
2026-08-08 20:52:02 +0300

    fix 2

--- i2c.py
+++ i2c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from smbus2 import SMBus, i2c_msg
+from time import sleep
 
 # some constants
 I2C_ADDR = 0x62
@@ -25,4 +26,41 @@ def sensirion_common_generate_crc(data: list[int]) -> int:
     return crc
 
 
-_ = i2c_msg.write(I2C_ADDR, CMD_READ_MEASUREMENT)
+def read_measurement():
+    with SMBus(1) as bus:
+        
+        write_frame = i2c_msg.write(I2C_ADDR, CMD_READ_MEASUREMENT)
+        print("create write_frame:", write_frame)
+        bus.i2c_rdwr(write_frame)
+        print("write_frame sent.")
+        
+        sleep(1)
+        print("Sleep passed.")
+
+        read_frame = i2c_msg.read(I2C_ADDR, 9)
+        print("create read_frame:", read_frame)
+        bus.i2c_rdwr(read_frame)
+        print("read_frame sent.")
+
+        data_bytes = list(read_frame)
+        print(data_bytes)
+
+        # will skip CRC verification for now
+        co2_result = data_bytes[0:2]
+        temp_result = data_bytes[3:5]
+        humidity_result = data_bytes[6:8]
+
+        raw_co2 = (co2_result[0] << 8) | co2_result[1]
+        raw_temp = (temp_result[0] << 8) | temp_result[1]
+        raw_humidity = (humidity_result[0] << 8) | humidity_result[1]
+
+        co2_ppm = raw_co2
+        temperature_c = -45 + 175 * raw_temp / (2**16 - 1)
+        humidity = 100 * raw_humidity / (2**16 - 1)
+
+        result = list([co2_ppm, temperature_c, humidity])
+        print(result)
+
+        return result
+
+_ = read_measurement()
